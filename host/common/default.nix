513ee55b505e9ccb9fd3fead7876ebc9341da292
Eddie Hickey <erhickey@gmail.com>
2023-09-29 18:35:59 -0400

    enable dark mode

--- host/common/default.nix
+++ host/common/default.nix
@@ -64,6 +64,20 @@
     (nerdfonts.override { fonts = [ "LiberationMono" ]; })
   ];
 
+  environment.etc = {
+    "xdg/gtk-2.0/gtkrc".text = ''
+      gtk-application-prefer-dark-theme=true
+    '';
+    "xdg/gtk-3.0/settings.ini".text = ''
+      [Settings]
+      gtk-application-prefer-dark-theme=true
+    '';
+    "xdg/gtk-4.0/settings.ini".text = ''
+      [Settings]
+      gtk-application-prefer-dark-theme=true
+    '';
+  };
+
   environment.systemPackages = with pkgs; [
     bat
     bc
